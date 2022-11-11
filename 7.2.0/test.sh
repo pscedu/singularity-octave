d45ab085be60f25c80374b520c0297fe6042fd0f
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2022-11-11 18:55:10 -0500

    Added version 7.2.0

---
+++ 7.2.0/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+singularity exec singularity-octave-7.2.0.sif octave-cli --help
+singularity exec singularity-octave-7.2.0.sif pandoc --help
+singularity exec singularity-octave-7.2.0.sif gnuplot --help
